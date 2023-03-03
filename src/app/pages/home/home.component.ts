a4416bd49fc6ede02e0abe207d9f1ab221ea6b0c
LuizRissardi <rissardi.luiz2006@gmail.com>
2023-03-03 17:51:11 -0300

    elaboraçãod e responsivo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 //angular
-import { Component, EventEmitter, Inject, OnInit } from '@angular/core';
+import { Component, EventEmitter, Inject, OnInit, Renderer2 } from '@angular/core';
 import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { io } from "socket.io-client";
@@ -28,8 +28,11 @@ export class HomeComponent implements OnInit {
   Chat: any;
   Form: FormGroup;
   ChatIdAtual: number = 0;
-  Socket : any;
-  constructor(private formulario: FormBuilder, private modalConfig: NgbModalConfig, private modal: NgbModal, @Inject(EmiterService) private emiter: EmiterInterface, @Inject(FetchChatService) private ApiChat: FetchChatInterface, @Inject(FetchApiService) private ApiService: FetchApiInterface) {
+  SocketChat: any;
+  SocketClient: any;
+  mobileon:boolean = false
+
+  constructor(private render: Renderer2, private formulario: FormBuilder, private modalConfig: NgbModalConfig, private modal: NgbModal, @Inject(EmiterService) private emiter: EmiterInterface, @Inject(FetchChatService) private ApiChat: FetchChatInterface, @Inject(FetchApiService) private ApiService: FetchApiInterface) {
     this.Usuario = emiter.Subscribe();
     this.modalConfig.backdrop = "static"
     this.Form = this.formulario.group({
@@ -38,28 +41,55 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.Socket = io("https://chat-api.herokuapp.com/", {
+    console.log(this.Usuario)
+    this.SocketChat = io("https://chat-api.herokuapp.com/", {
       transports: ['websocket']
-   })
+    })
 
-   this.Socket.on("message",(data:any)=>{
-    this.Chat.chat[0].chats = data;
-  })
+    this.SocketClient = io("http://localhost:3000", {
+      transports: ['websocket']
+    });
+
+    this.SocketClient.emit("connectRoom", this.Usuario.phone);
+
+    this.SocketClient.on("subscribeChat", (chat: any) => {
+      console.log("chat =>", chat)
+      console.log("use =>", this.Usuario)
+      this.Usuario.chats.push(chat)
+    })
+
+    this.SocketChat.on("message", (data: any) => {
+      this.Chat.chat[0].chats = data;
+    })
   }
 
   open(modal: any) {
     this.modal.open(modal)
   }
 
+  voltar(){
+    this.Chat = false;
+    this.mobileon = false
+  }
+
 
   getChat(chatId: number, phoneUser: number) {
     this.ChatIdAtual = chatId;
-    this.Socket.emit("connectRoom",this.ChatIdAtual)
+    this.SocketChat.emit("connectRoom", this.ChatIdAtual)
     this.ApiChat.getchat(chatId).subscribe((data: any) => {
       this.Chat = data;
       this.Chat.phoneUser = Number(phoneUser);
+      console.log(window.screen.width)
       this.Chat.yourphone = this.Usuario.phone;
-      console.log(this.Chat)
+      if (window.screen.width < 560) {
+        const dom = document.getElementById("chats")
+        if (dom) {
+          dom.classList.remove("col-8")
+          dom.style.position = "absolute";
+          dom.style.width = "100%";
+          this.mobileon = true
+        }
+      }
     })
   }
 
@@ -68,9 +98,24 @@ export class HomeComponent implements OnInit {
     const phone2 = Number(this.Form.get("phone1")?.value);
     this.ApiChat.CreateChat().subscribe((chatId: any) => {
       if (chatId.id) {
-        console.log(chatId.id)
         this.ApiService.InitChat(phone1, phone2, chatId.id).subscribe(data => {
-
+          console.log("usuario =>", this.Usuario)
+          console.log("data =>", data)
+          this.Usuario = data.dados;
+          const chatUser = {
+            chatId: chatId.id,
+            person: {
+              nome: this.Usuario.nome,
+              phone: this.Usuario.phone
+            },
+            dateDelet: null
+          }
+          this.SocketClient.emit("newChat", chatUser, phone2)
+          this.ApiChat.getchat(chatId).subscribe((data: any) => {
+            this.Chat = data;
+            this.Chat.phoneUser = Number(phone2);
+            this.Chat.yourphone = this.Usuario.phone;
+          })
         })
       }
     })
@@ -82,7 +127,7 @@ export class HomeComponent implements OnInit {
     this.Chat.chat[0].chats.push(messager)
     this.ApiChat.SendMessage(this.Chat.chat[0].chats, this.ChatIdAtual).subscribe(data => {
       console.log(data)
-      this.Socket.emit("sendMessage",this.Chat.chat[0].chats)
+      this.SocketChat.emit("sendMessage", this.Chat.chat[0].chats, this.ChatIdAtual)
     })
   }
 }
